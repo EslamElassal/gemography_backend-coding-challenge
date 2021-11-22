d93fa7b5b0fc2b737d5ebeeabf6e0611ffed2d2b
eslam elassal <eslamengineermtx@gmail.com>
2021-11-22 23:53:54 +0200

    Create User Model

---
+++ gemography_backend-coding-challenge/Models/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace gemography_backend_coding_challenge.Models
+{
+    public class User
+    {
+        public long ID { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public byte[] PasswordSalt { get; set; }
+        public byte[] PasswordHash { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string MobileNumber { get; set; }
+        public string Address { get; set; }
+
+    }
+}
