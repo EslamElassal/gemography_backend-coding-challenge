d112ab4c7d0871edbd39f9bf1c36372e858f6d7b
eslam elassal <eslamengineermtx@gmail.com>
2021-11-22 23:55:00 +0200

    Create Token Service and Interface

---
+++ gemography_backend-coding-challenge/Token/ITokenService.cs
@@ -0,0 +1,15 @@
+﻿using gemography_backend_coding_challenge.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VDream.Bll.Token
+{
+ 
+    public interface ITokenService
+    {
+        string CreateToken( User user);
+
+    }
+}
