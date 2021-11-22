551d0fa9c68bb4ae120bfe75973403ec07d972c4
eslam elassal <eslamengineermtx@gmail.com>
2021-11-22 19:33:06 +0200

    RepoLanguagesController Created

---
+++ gemography_backend-coding-challenge/Controllers/RepoLanguagesController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace gemography_backend_coding_challenge.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RepoLanguagesController : ControllerBase
+    {
+    }
+}
